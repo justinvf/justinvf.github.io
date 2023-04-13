301f0cb9af27f5b7abaa1c4c0ec0d6915caf9240
Justin Vincent <justinvf@pop-os.localdomain>
2023-04-13 07:58:46 -0400

    WIP resume

--- make_resume.py
+++ make_resume.py
@@ -9,12 +9,56 @@ template_str = """
 <html>
 <head>
     <title>{{ name }}'s Resume</title>
+    <style>
+    body {
+      background-color: #f2f2f2;
+    }
+    </style>
 </head>
 <body>
+
+<h1>
+    {{ name }}
+</h1>
+<div class="contact">
+email: {{ email }}
+<br>
+{{ location }}
+<br>
+<hr/>
+
+<div>
+    <h2>Experience</h2>
     {% for job in jobs %}
-        <h2>{{ job.company }}</h2>
+        <h2>{{ job.company }}</h3>
         <p>{{ job.title }}</p>
+        <ul>
+        {% for detail in job.details  %}
+          <li> {{ detail }} </li>
+        {% endfor %}
+        </ul>
     {% endfor %}
+
+<hr/>
+</div>
+
+<div>
+    <h2>Education</h2>
+    {% for school in education.schools %}
+        <h2>{{ school.name }}</h3>
+        {{ school.gpa }} GPA
+        {% for degree in school.degrees  %}
+        <h3> {{ degree.degree }} </h3>
+          <ul>
+          {% for detail in degree.details  %}
+            <li>{{ detail }}</li>
+          {% endfor %}
+          </ul>
+        {% endfor %}
+    {% endfor %}
+</div>
+
+
 </body>
 </html>
 """
