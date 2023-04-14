0d725b87ab219796353a57333f07e994e85d6762
Justin Vincent <justinvf@pop-os.localdomain>
2023-04-14 00:54:06 -0400

    Start of grid layout

--- make_resume.py
+++ make_resume.py
@@ -4,66 +4,11 @@ import json
 with open('resume.json', 'rt') as f:
     resume_data = json.load(f)
 
-template_str = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{ name }}'s Resume</title>
-    <style>
-    body {
-      background-color: #f2f2f2;
-    }
-    </style>
-</head>
-<body>
-
-<h1>
-    {{ name }}
-</h1>
-<div class="contact">
-email: {{ email }}
-<br>
-{{ location }}
-<br>
-<hr/>
-
-<div>
-    <h2>Experience</h2>
-    {% for job in jobs %}
-        <h2>{{ job.company }}</h3>
-        <p>{{ job.title }}</p>
-        <ul>
-        {% for detail in job.details  %}
-          <li> {{ detail }} </li>
-        {% endfor %}
-        </ul>
-    {% endfor %}
-
-<hr/>
-</div>
-
-<div>
-    <h2>Education</h2>
-    {% for school in education.schools %}
-        <h2>{{ school.name }}</h3>
-        {{ school.gpa }} GPA
-        {% for degree in school.degrees  %}
-        <h3> {{ degree.degree }} </h3>
-          <ul>
-          {% for detail in degree.details  %}
-            <li>{{ detail }}</li>
-          {% endfor %}
-          </ul>
-        {% endfor %}
-    {% endfor %}
-</div>
-
-
-</body>
-</html>
-"""
+with open('resume_jinja.html', 'rt') as f:
+    template_str = f.read();
 
 template = Template(template_str)
+
 html = template.render(resume_data)
 
 with open('resume.html', 'wt') as f:
